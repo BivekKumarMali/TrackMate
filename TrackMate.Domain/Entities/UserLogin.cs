f2b0480e63474a0f43dcf2a6acea4e56c0082964
BivekKumarMali <mike.shane23@gmail.com>
2023-07-04 09:35:54 +0530

    Adds identity login user model

---
+++ TrackMate.Domain/Entities/UserLogin.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace TrackMate.Domain.Entities
+{
+    public class UserLogin : IdentityUserLogin<string>
+    {
+        [Key]
+        public int Id { get; set; }
+        public DateTime LoginTime { get; set; }
+    }
+}
